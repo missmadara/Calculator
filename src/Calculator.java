a8ab509a70ee5c7b3109fbf726e2bb1afa1585fc
missmadara <vanaga33@gmail.com>
2021-03-04 21:49:04 +0000

    My second try for to add smt to the git

---
+++ src/Calculator.java
@@ -0,0 +1,27 @@
+import javax.xml.transform.Result;
+//Calculator calculator = new Calculator();
+//Int num1 = 5;
+//Int num2 = 3;
+//Int result = calculator.add(num1, num2);
+//system.out.println(num1 + num2 + “ = ” + result)//
+
+public class Calculator {
+    public static void someCalculations() {
+        int num1 = 20;
+        int num2 = 30;
+        int num3 = 99;
+
+        int result = num1 + num3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num3 - num1);
+        System.out.println(num1 * num2);
+        System.out.println(num2 / num1);
+        System.out.println(Math.max(num1, num2));
+        System.out.println(Math.min(num1, num3));
+        System.out.println(num1 + num2 +"=" + result);
+    }
+    }
+
+
+
---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+    public static void main(String[] arg) {
+    Calculator calculator = new Calculator();
+    calculator.someCalculations();
+    }
+    //Calculator calculator = new Calculator();
+    //Int num1 = 5;
+    //Int num2 = 3;
+    //Int result = calculator.add(num1, num2);
+    //system.out.println(num1 + num2 + “ = ” + result)
+
+}
+
